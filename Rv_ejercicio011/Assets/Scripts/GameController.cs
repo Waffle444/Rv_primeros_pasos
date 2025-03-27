ba1c9b45fd4d3b6df9807efe645b0b147b9da1af
Andre <arevillaroque@gmail.com>
2025-03-26 20:17:30 -0500

    Ejercicio03_pingpong
    Creación de un juego de pingpong en VR , use dos tipos de punteros uno que es gazemanager el puntito rosa de los anteriores ejercicios y la raqueta de tenis

---
+++ Rv_ejercicio011/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public Player player;
+	public Ball ball;
+	public TextMesh scoreText;
+
+	private float gameOverTimer = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool isGameOver = ball.transform.position.z < player.transform.position.z;
+
+		if (isGameOver == false) {
+			scoreText.text = "Score: " + ball.score;
+		} else {
+			scoreText.text = "Game over!\nYour final score: " + ball.score;
+
+			gameOverTimer -= Time.deltaTime;
+			if (gameOverTimer <= 0f) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+			}
+		}
+	}
+}
