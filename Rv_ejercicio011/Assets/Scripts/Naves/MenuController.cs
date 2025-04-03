2990d418c1eb6d7861409424b6a476f92d5cf48c
Andre <arevillaroque@gmail.com>
2025-04-02 20:18:53 -0500

    Puntuaciones vasitos y avance de naves
    Usando playerprefs creo acertadas y no acertadas y avance de las naves mapa y arma y scripts

---
+++ Rv_ejercicio011/Assets/Scripts/Naves/MenuController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour
+{
+    private Camera xRCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRCamera = CameraPointerManager.instance.gameObject.GetComponent<Camera>();
+    }
+
+    public void menu_principal()
+    {
+        SceneManager.LoadScene("Menu_principalNaves");
+    }
+
+    public void reiniciar()
+    {
+        SceneManager.LoadScene("Ejercicio05-naves");
+    }
+
+    public void game_over()
+    {
+        SceneManager.LoadScene("Menu_Game_OverNaves");
+    }
+
+    public void OnPointerClick()
+    {
+        PointerEventData pointerEvent = PlacePointer(); // Este es el elemento que nos permitira hacer clic sobre el elemento UI
+        ExecuteEvents.Execute(this.gameObject, pointerEvent, ExecuteEvents.pointerClickHandler); // Ejecutamos el Evento al hacer clic
+    }
+
+    public PointerEventData PlacePointer()
+    {
+        Vector3 screePos = xRCamera.WorldToScreenPoint(CameraPointerManager.instance.hitPoint);
+        var pointer = new PointerEventData(EventSystem.current);
+        pointer.position = new Vector2(screePos.x, screePos.y);
+        return pointer;
+    }
+}
--- Rv_ejercicio011/Assets/Scripts/Vasitos/GameController.cs
+++ Rv_ejercicio011/Assets/Scripts/Vasitos/GameController.cs
@@ -2,6 +2,7 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameController : MonoBehaviour {
 
@@ -11,25 +12,42 @@ public class GameController : MonoBehaviour {
     public Cup[] cups;
 
     private float resetTimer = 3f;
-
-    // Use this for initialization
+    [SerializeField] 
+    private TextMeshProUGUI Ganadas;
+    [SerializeField]
+    private TextMeshProUGUI Perdidas;
+    public int win;
+    // Use this fors initialization
     void Start () {
         infoText.text = "Escoje la opcion correcta";
 
         StartCoroutine (ShuffleRoutine());
+        Ganadas.text ="acertadas:"+ PlayerPrefs.GetInt("acertadas", 0).ToString();
+        Perdidas.text ="no_acertadas"+ PlayerPrefs.GetInt("no_acertadas", 0).ToString();
     }
     
     // Update is called once per frame
     void Update () {
+
         if (player.picked) {
             if (player.won) {
                 infoText.text = "Ganaste!";
+                win = 1;
             } else {
                 infoText.text = "Perdiste intenta de nuevo!";
+                win = 0;
             }
 
             resetTimer -= Time.deltaTime;
             if (resetTimer <= 0f) {
+                if (win >0)
+                {
+                    PlayerPrefs.SetInt("acertadas", PlayerPrefs.GetInt("acertadas", 0) + 1);
+                }
+                else
+                {
+                    PlayerPrefs.SetInt("no_acertadas", PlayerPrefs.GetInt("no_acertadas", 0) + 1);
+                }
                 SceneManager.LoadScene (SceneManager.GetActiveScene().name);
             }
         }
